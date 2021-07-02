8b611836748127b88e80ab902318deda8a7d75ed
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-07-02 15:33:59 +0100

    Gutter ball test pass - total score = 0

--- src/main/java/bowlingexercise3/Bowling.java
+++ src/main/java/bowlingexercise3/Bowling.java
@@ -2,6 +2,38 @@ package bowlingexercise3;
 
 public class Bowling {
 
+    private int throww = 0;
+    private int[] throwws = new int[28];
+
+    public void throww(String s) {
+    }
+
+    public void throwws(String totalScore) {
+
+        for (int i=0; i< totalScore.length(); i++) {
+
+            if (totalScore.charAt(i) == '|') {
+
+            } else {
+                int x = totalScore.charAt(i);
+                throwws[throww++] = x - '0';
+            }
+        }
+    }
+
+    public int score() {
+        int score = 0;
+        int cursor = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            score += throwws[cursor] + throwws[cursor+1];
+            cursor += 2;
+            }
+
+        return score;
+    }
+
+
+
 
     public void roll(){
 
@@ -12,4 +44,6 @@ public class Bowling {
 
         return 0;
     }
+
+
 }
--- src/test/java/bowlingexercise3/BowlingTest.java
+++ src/test/java/bowlingexercise3/BowlingTest.java
@@ -3,8 +3,14 @@ package bowlingexercise3;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 public class BowlingTest {
+
+
+    //private Game game;
 
     @Test
     public void createGame() {
@@ -12,5 +18,11 @@ public class BowlingTest {
 
     }
 
+    @Test
+    public void noScore(){
+        Bowling game = new Bowling();
+        game.throww("00|00|00|00|00|00|00|00|00|00");
+        assertThat(game.score(), is(0));
+    }
 
 }
