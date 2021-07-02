67137633a26df7103569bcb74e57d58e749474c6
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-07-02 14:58:39 +0100

    Added dependencies and initial test

---
+++ src/main/java/bowlingexercise3/Bowling.java
@@ -0,0 +1,15 @@
+package bowlingexercise3;
+
+public class Bowling {
+
+
+    public void roll(){
+
+    }
+
+
+    public int game(){
+
+        return 0;
+    }
+}
---
+++ src/test/java/bowlingexercise3/BowlingTest.java
@@ -0,0 +1,16 @@
+package bowlingexercise3;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class BowlingTest {
+
+    @Test
+    public void createGame() {
+        Bowling game = new Bowling();
+
+    }
+
+
+}
