6eb9ff5ed5c2d75623c92a32df50dd06bf97939d
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-07-05 10:19:46 +0100

    Refactoring

--- src/test/java/bowlingexercise3/BowlingTest.java
+++ src/test/java/bowlingexercise3/BowlingTest.java
@@ -7,34 +7,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+
 public class BowlingTest {
 
-@Test
-public void scoreZero(){
-    Bowling game = new Bowling();
-    game.throwws("00|00");
-    assertThat(game.score(), is(0));
-}
+    @Test
+    public void scoreZero() {
+        Bowling game = new Bowling();
+        game.throwws("00|00");
+        assertThat(game.score(), is(0));
+    }
 
 
     @Test
-    public void scoreTwo(){
+    public void score_7() {
         Bowling game = new Bowling();
-        game.throwws("10|10|00|00|00|00|00|00|00|00");
-        assertThat(game.score(), is(2));
+        game.throwws("10|10|10|10|10|10|10|00|00|00");
+        assertThat(game.score(), is(7));
     }
 
     @Test
-    public void score8(){
+    public void score_8() {
         Bowling game = new Bowling();
         game.throwws("80|00");
         assertThat(game.score(), is(8));
     }
 }
-
-//    @Test
-//    public void score8(){
-//        Bowling game = new Bowling();
-//        game.throwws("18|18|90|90|50|90|90|00|00|00");
-//        assertThat(game.score(), is(60));
-//    }
