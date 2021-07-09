cbd2f4a7ec3364b244acde05d6fbcd5282598d0e
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-07-09 15:41:53 +0100

    Grouped Tests

--- src/test/java/bowlingexercise3/BowlingTest.java
+++ src/test/java/bowlingexercise3/BowlingTest.java
@@ -17,16 +17,31 @@ public class BowlingTest {
     @ParameterizedTest
 
     @CsvSource(value = {
-
+            //Some combinations of numbers to 10
             "--|, 0", "1-|, 1", "11|, 2", "12|, 3", "4-|, 4","32|, 5","33|, 6",
-            "61|, 7","44|, 8","45|, 9","44|2-, 10", "-/|--, 10",
+            "61|, 7","44|, 8","45|, 9","44|2-, 10", "-/|--, 10"})
 
+    public void testBowling(String turns, int score) {
+        Bowling game = new Bowling();
+        game.turns(turns);
+        assertEquals(score, game.score());
+    }
+
+
+    @ParameterizedTest
+
+    @CsvSource(value = {
+
+            //testing of numbers greater than 10, including spare tests
+            "9-|2-, 11",
+            "-/|43|-8|25|53|-4|45|-7|-/, 74",
             "-/|43|-/|-/|-/|-/|45|-/|-/, 94",
             "-/|9/|-/|-/|5/|-/|45|-/|-/, 107",
+            "-/|9/|-/|-/|5/|-7|45|9/|-/, 100",
             "-/|-/|-/|-/|-/|-/|-/|7/|-/, 97",
             "1/|-/|4/|9/|, 53"})
 
-    public void testBowling(String turns, int score) {
+    public void testBowlingSpare(String turns, int score) {
         Bowling game = new Bowling();
         game.turns(turns);
         assertEquals(score, game.score());
