d8308dc5d5facaeb2a1ba5ffce74feb7c48ac317
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-07-28 11:41:01 +0100

    Scanner reads in bowling string and outputs actual score!

--- src/main/java/bowlingexercise3/BowlingApplication.java
+++ src/main/java/bowlingexercise3/BowlingApplication.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class BowlingApplication {
 
-    public static void main (String args[]){
+    public static void main(String args[]) {
 
-//        //use scanner to read from console
         Scanner scanner = new Scanner(System.in);
-//        //read input from tests
-//        //call the bowling class with user input
-//        //print score to console
 
-        System.out.println("Welcome to 10 Pin Bowling!");
+        System.out.println("Welcome to 10 Pin Bowling! " +
+                "Please enter game score in String format: ");
 
         String score = scanner.nextLine();
 
+        Bowling game = new Bowling();
+        game.turns(score);
+
+        System.out.println("The String is: " + game.score());
 
     }
 }
