b5c8db7a40d2db8957b876c922968dbc27aedccf
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-07-29 11:59:43 +0100

    Included a termination method for cmd line

--- src/main/java/bowlingexercise3/Bowling.java
+++ src/main/java/bowlingexercise3/Bowling.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 
 public class Bowling {
 
+    //Determines the number of rolls and limits the frames to 10
+    private int turn = 0;
+    //limits the frames
+    private int[] turns = new int[24];
     // Regular Expressions
     private static String REGEX_SPARES_STRIKES = "(([-1-9][-1-9/]|[X])\\|){10}\\|";
     private static String REGEX_BONUS = "$";
@@ -12,10 +16,6 @@ public class Bowling {
     private static String REGEX_BONUS_STRIKE = "(?<=X\\|\\|)(XX|X[- 1-9]|[- 1-9][- 1-9/])";
     private static String REGEX_VALID_FULL = REGEX_SPARES_STRIKES + "(" + REGEX_BONUS + "|" + REGEX_BONUS_SPARE + "|" + REGEX_BONUS_STRIKE + ")";
     private static Pattern REGEX_VALID_EXP = Pattern.compile(REGEX_VALID_FULL);
-    //Determines the number of rolls and limits the frames to 10
-    private int turn = 0;
-    //limits the frames
-    private int[] turns = new int[24];
 
     //Private Boolean created to keep track of pointers which correctly add scores
     private boolean strike(int cursor) {
@@ -36,9 +36,8 @@ public class Bowling {
             if (total.charAt(i) == 'X') {
                 turns[turn++] = 10;
                 //Incorporates the addition/separation of the pipe (|) symbol
-            } if (total.charAt(i) != '|') {
+            } else if (total.charAt(i) == '|') {
                 //Incorporates the addition of a miss (-) symbol
-                continue;
             } else if (total.charAt(i) == '-') {
                 turns[turn++] = 0;
                 //Incorporates the addition of a spare (/) symbol
@@ -52,6 +51,7 @@ public class Bowling {
                 turns[turn++] = x - '0';
             }
         }
+
     }
 
     private void validateInput(String input) throws IllegalArgumentException {
@@ -88,7 +88,6 @@ public class Bowling {
                 cursor += 2;
             }
         }
-
         return score;
     }
 }
--- src/main/java/bowlingexercise3/BowlingApplication.java
+++ src/main/java/bowlingexercise3/BowlingApplication.java
@@ -1,22 +1,44 @@
 package bowlingexercise3;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class BowlingApplication {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        char press, clear;
+        int i = 0;
 
-        System.out.println("Welcome to 10 Pin Bowling! " +
-                "Please enter game score in String format: \n");
+        while (i < 10) {
 
-        String score = scanner.nextLine();
-        Bowling game = new Bowling();
-        game.turns(score);
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("The total score for this game is: \n");
-        System.out.println();
-        System.out.println(game.score());
+            System.out.println("Welcome to 10 Pin Bowling!");
+            System.out.println();
+            System.out.println("Please enter game score in String format: \n");
+
+            String score = scanner.nextLine();
+            Bowling game = new Bowling();
+            game.turns(score);
+
+            System.out.println("The total score for this game is: \n");
+            System.out.println(game.score());
+            System.out.println();
+            System.out.println("Press any key to continue, otherwise enter t to terminate: ");
+
+            press = (char) System.in.read();
+            System.out.println("You Pressed: " + press);
+
+            do {
+                clear = (char) System.in.read();
+            } while (clear != '\n');
+            System.out.println("\n");
+
+            if (press == 't' | press == 'T') {
+                System.out.println("Game Will Now Terminate......Thank You For Playing 10 Pin Bowling");
+                break;
+            }
+        }
     }
 }
